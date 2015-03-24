3a6e2f31bd812c827106ca2deae8af54f7fbdb8c
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-03-24 11:27:15 -0300

    old

--- 20150317.hs
+++ 20150317.hs
@@ -1,3 +1,50 @@
+-- Quicksort
+
+bigOfTwo :: Int -> Int -> Int
+bigOfTwo a b
+	| a > b  = a
+	| otherwise  = b
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+-- Sum digits
+sumd 0 = 0
+sumd x = (x `mod` 10) + sumd (x `div` 10)
+
+ord :: [Int] -> [Int]
+ord [] = []
+ord [x] = [sumd x]
+ord xs@(x:xt) = [sumd x] ++ ord(xt)
+
+ordenar :: [Int] -> [Int]
+ordenar xs = quicksort (ord xs)
+
+fibonacci :: Int -> Int
+fibonacci 0 = 1
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+getEvens :: [Int] -> [Int]
+getEvens [] = []
+getEvens (a:x)
+ | mod a 2 == 0 = a:getEvens x
+ | otherwise = getEvens x
+
+fiblist :: Int -> [Int]
+fiblist a = [fibonacci a] ++ fiblist(a-1)
+
+{-
+
+
+Learning Material: not from class
+
+-}
+
 -- My not function
 myNot :: Bool -> Bool
 myNot True = False
@@ -46,7 +93,36 @@ contains y (a:x)
 	| y == a  = True
 	| otherwise = contains y x
 
-
 count :: [t] -> Int
 count [] = 0
 count (a:x) = 1 + count x
+
+search :: Int -> Int -> Bool
+search a i
+ | i * i > a = True
+ | mod a i == 0 = False
+ | otherwise = search a (i+1)
+
+isPrime :: Int -> Bool
+isPrime a
+ | a <= 1 = False
+ | otherwise = search a 2
+
+equals :: [Int] -> [Int] -> Bool
+equals [] [] = True
+equals _ [] = False
+equals [] _ = False
+equals (h:x) (g:y)
+ | h == g = equals x y
+ | otherwise = False
+
+countOcurrences :: Int -> [Int] -> Int
+countOcurrences _ [] = 0
+countOcurrences y (a:x)
+	| y == a  = 1 + countOcurrences y x
+	| otherwise = countOcurrences y x
+
+bigger :: [Int] -> Int
+bigger (a:x)
+ | count (a:x) == 1  = a
+ | otherwise  = biggerOfTwo a (bigger x)
