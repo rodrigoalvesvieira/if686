571451a39ab0a14ea51bee97e0cc0fcdca55d51a
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-03-24 00:42:21 -0300

    A few more functions

--- 20150317.hs
+++ 20150317.hs
@@ -11,11 +11,42 @@ incr e = (+) e 1
 sumLists :: [Int] -> [Int] -> [Int]
 sumLists lista listb = (++) lista listb
 
-isDigit :: Char -> Bool
-isDigit ch = ch >= '0' && ch <= '9'
+-- Smaller of two integers
+smallerOfTwo :: Int -> Int -> Int
+smallerOfTwo a b
+	| a < b  = a
+	| otherwise  = b
 
-digits :: String -> [Bool]
-digits str
-  | (==) (length str) 0 = []
-  | (==) (length str) 1 = isDigit (head str) : []
-  | otherwise = isDigit (head str) : (digits (tail str))
+-- Bigger of two integers
+biggerOfTwo :: Int -> Int -> Int
+biggerOfTwo a b
+	| a > b  = a
+	| otherwise  = b
+
+-- Smaller of three integers
+smallerOfThree :: Int -> Int -> Int -> Int
+smallerOfThree a b c
+     | a < (smallerOfTwo b c) = a
+	 | otherwise = (smallerOfTwo b c)
+
+-- Bigger of three integers
+biggerOfThree :: Int -> Int -> Int -> Int
+biggerOfThree a b c
+     | a > (biggerOfTwo b c) = a
+   | otherwise = (biggerOfTwo b c)
+
+factorial :: Int -> Int
+factorial 0  = 1
+factorial n  = n * factorial (n-1)
+
+-- Find out if an integer is within a list
+contains :: Int -> [Int] -> Bool
+contains _ [] = False
+contains y (a:x)
+	| y == a  = True
+	| otherwise = contains y x
+
+
+count :: [t] -> Int
+count [] = 0
+count (a:x) = 1 + count x
