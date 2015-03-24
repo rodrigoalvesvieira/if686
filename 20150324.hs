a25c99fed72ce9c59f30456ead6f2976422cbdb1
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-03-24 11:05:13 -0300

    Finishing first two class exercises

--- 20150324.hs
+++ 20150324.hs
@@ -1,16 +1,8 @@
 {-
 
-Quicksort
+Funções do trabalho
 
 -}
-
-quicksort :: [Int] -> [Int]
-quicksort [] = []
-quicksort (x:xs) =
-    let smallerSorted = quicksort [a | a <- xs, a <= x]
-        biggerSorted = quicksort [a | a <- xs, a > x]
-    in  smallerSorted ++ [x] ++ biggerSorted
-
 
 -- Mergesort
 
@@ -37,3 +29,47 @@ mergesort :: [Int] -> [Int]
 mergesort [x] = [x]
 mergesort xs  = let (as, bs) = split xs
                 in merge (mergesort as) (mergesort bs)
+
+
+{-
+
+Funções da aula
+
+-}
+
+-- 1. Defina a função *menorMaior* que reebe três inteiros e retorna uma tupla com o menor e o maior deles.
+menorDeDois :: Int -> Int -> Int
+menorDeDois a b
+	| a < b  = a
+	| otherwise  = b
+
+maiorDeDois :: Int -> Int -> Int
+maiorDeDois a b
+	| a > b  = a
+	| otherwise  = b
+
+menorDeTres :: Int -> Int -> Int -> Int
+menorDeTres a b c
+     | a < (menorDeDois b c) = a
+	 | otherwise = (menorDeDois b c)
+
+maiorDeTres :: Int -> Int -> Int -> Int
+maiorDeTres a b c
+      | a > (maiorDeDois b c) = a
+ 	 | otherwise = (maiorDeDois b c)
+
+menorMaior :: Int -> Int -> Int -> (Int, Int)
+menorMaior a b c = (menor, maior)
+  where
+    menor = menorDeTres a b c
+    maior = maiorDeTres a b c
+
+-- 2. Defina a função *ordenaTripla* que recebe uma tripla de inteiros e ordna a mesma
+
+ordenaTripla :: (Int, Int, Int) -> (Int, Int, Int)
+ordenaTripla (a, b, c) = (x, y, z)
+  where
+    list = mergesort [a, b, c]
+    x = list!!0
+    y = list!!1
+    z = list!!2
